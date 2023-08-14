ec9f8650edb1b5f1cca7e5ae0ab9655a58ed3363
yvess <yvess.straten@gmail.com>
2023-08-14 14:50:42 +0800

    Added attribution

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Buttons from "./components/Buttons";
 function App() {
   return (
     <div className="animate-fade-in">
-      <div className="w-full mx-auto text-center">
-        <h1 className="text-pink-200 font-bold pb-1">Drum Machine</h1>
+      <div className="w-full mx-auto text-center pb-2">
+        <h1 className="text-pink-200 font-bold">Drum Machine</h1>
+         <h3 className="text-pink-200 text-xs font-sans font-bold">
+            Thank you K-dev on youtube for the inspiration          </h3>
+	  <h3 className="text-pink-200 text-xs font-sans-font-bold">  to use
+             <code>Array.map</code> for the pads in my code
+	      </h3>
+
       </div>
       <div
         id="drum-machine"
